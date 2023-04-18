0cfd58ae2145e73d5cb7d0012503e9154e1225fa
castorm <theiliyastorm@gmail.com>
2023-04-18 12:35:15 -0700

    a

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title> School Database Sample </title>
+<link href="css/style.css" rel="stylesheet"/>
+</head>
+<body>
+<div id="wrapper">
+    <div id="row1">
+        <div id="row2_col2">
+            <form action="" method="post">
+                
+</body>
+</html>
